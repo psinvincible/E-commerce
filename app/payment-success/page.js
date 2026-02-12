7e500f1d75b989558a75963fafe8176c32111340
psinvincible <sintujikaiseho@gmail.com>
2026-02-12 12:33:57 +0530

    minor changes

--- app/payment-success/page.js
+++ app/payment-success/page.js
@@ -1,6 +1,7 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 
-"use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
