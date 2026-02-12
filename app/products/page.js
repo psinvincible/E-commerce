2f28a005bf3ddd2d85ad3cd7d20587855ef0c213
psinvincible <sintujikaiseho@gmail.com>
2026-02-12 12:42:45 +0530

    build error - force-dynamic in product

--- app/products/page.js
+++ app/products/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import BreadCrumbs from "@/components/BreadCrumbs";
 import CategorySidebar from "@/components/products/Category";
 import ProductGrid from "@/components/products/ProductGrid";
